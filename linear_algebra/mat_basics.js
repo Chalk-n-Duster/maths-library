947e2c8ea1ab0d4746239cb09bb326a9061e2579
Aritra Chatterjee <aritrachatterjee07@gmail.com>
2020-05-20 12:44:14 +0530

    Added transpose,trace,adjoint in mat_basics

--- linear_algebra/mat_basics.js
+++ linear_algebra/mat_basics.js
@@ -1,5 +1,5 @@
 const nerdamer=require('nerdamer')
-const {stringify,matrixTex,replaceAll}=require('./utils')
+const {stringify,matrixTex,replaceAll,stringTeX}=require('./utils')
 
 
 const buildFromFunc=(fun,x,y)=>{
@@ -47,6 +47,72 @@ const buildFromFunc=(fun,x,y)=>{
 
 }
 
+const transpose=(mat)=>{
+    if(typeof(mat)==='string')
+    var nerdmat=mat
+    else
+    var nerdmat=stringify(mat)
+    nerdamer.setVar('M',nerdmat)
+    return {'tp':nerdamer('transpose(M)').toString(),'tex':nerdamer('transpose(M)').toTeX()}
+
+}
+
+const trace=(mat)=>{
+    const steps=[]
+    var st='tr(A)= '
+    var ans=0
+    for(var i=0;i<mat.length;i++){
+        if(i==mat.length-1)
+        st+=mat[i][i]
+        else
+        st+=(mat[i][i]+' + ')
+        ans+=mat[i][i]
+    }
+    steps.push(stringTeX(st))
+    steps.push(stringTeX('= '+ans))
+    return {'trace':ans,'tex':steps.join('\\\\')}
+}
+
+const adjoint=(mat,n)=>{
+    const steps=[]
+    const step1mat=[]
+    const step2mat=[]
+    for(var i=0;i<n;i++){
+        var step1matrow=[],step2matrow=[]
+        for(var j=0;j<n;j++){
+        
+        var cofmat=[]
+        for(var k=0;k<n;k++){
+            if(k==i)continue;
+            var cofmatrow=[]
+            for(var l=0;l<n;l++){
+                if(l==j)continue;
+                cofmatrow.push(mat[k][l])
+            }
+            cofmat.push(cofmatrow)
+        }
+        if((i+j)%2==0){
+            step1matrow.push('det'+matrixTex(cofmat))
+            step2matrow.push(nerdamer('determinant('+stringify(cofmat)+')').toString())
+        }
+        else{
+            step1matrow.push('(-)det'+matrixTex(cofmat))
+            step2matrow.push(nerdamer('determinant('+stringify(cofmat)+')').multiply(-1).toString())
+        }
+    }
+    step1mat.push(step1matrow)
+    step2mat.push(step2matrow)
+    }
+    steps.push(stringTeX('Calculating cofactor matrix:'))
+    steps.push(matrixTex(step1mat))
+    steps.push(stringTeX('After calculating all determinants,'))
+    steps.push(matrixTex(step2mat))
+    steps.push(stringTeX('Finally, adjoint of matrix is transpose of the cofactor matrix'))
+    steps.push(stringTeX('\\therefore adj(A)='))
+    var tr=transpose(step2mat)
+    steps.push(tr['tex'])
+    return {'adj':tr['tp'],'tex':steps.join('\\\\')}
+}
 
 
-module.exports={buildFromFunc}
+module.exports={buildFromFunc,transpose,trace,adjoint}
--- test.js
+++ test.js
@@ -1,79 +1,92 @@
-function determinant(matrix) {
-  const det = require("./linear_algebra/mat_det");
-  let x = det.get_determinant(matrix, 0, matrix.length, 1, "\\\\");
-  console.log(x);
-  /*
-  require("mathjax")
-    .init({
-      loader: { load: ["input/tex", "output/svg"] },
-    })
-    .then((MathJax) => {
-      const svg = MathJax.tex2svg(x, { display: true });
-      console.log(MathJax.startup.adaptor.outerHTML(svg));
-    })
-    .catch((err) => console.log(err.message));
-    */
-}
+// function determinant(matrix) {
+//   const det = require("./linear_algebra/mat_det");
+//   let x = det.get_determinant(matrix, 0, matrix.length, 1, "\\\\");
+//   console.log(x);
+//   /*
+//   require("mathjax")
+//     .init({
+//       loader: { load: ["input/tex", "output/svg"] },
+//     })
+//     .then((MathJax) => {
+//       const svg = MathJax.tex2svg(x, { display: true });
+//       console.log(MathJax.startup.adaptor.outerHTML(svg));
+//     })
+//     .catch((err) => console.log(err.message));
+//     */
+// }
 
-function cramer(matrix1, matrix2) {
-  const cr = require("./linear_algebra/mat_cramer");
-  let x = cr.get_cramer(matrix1, matrix2, matrix1[0].length);
-  console.log(x);
-  /*
-  require("mathjax")
-    .init({
-      loader: { load: ["input/tex", "output/svg"] },
-    })
-    .then((MathJax) => {
-      const svg = MathJax.tex2svg(x, { display: true });
-      console.log(MathJax.startup.adaptor.outerHTML(svg));
-    })
-    .catch((err) => console.log(err.message));
-    */
-}
+// function cramer(matrix1, matrix2) {
+//   const cr = require("./linear_algebra/mat_cramer");
+//   let x = cr.get_cramer(matrix1, matrix2, matrix1[0].length);
+//   console.log(x);
+//   /*
+//   require("mathjax")
+//     .init({
+//       loader: { load: ["input/tex", "output/svg"] },
+//     })
+//     .then((MathJax) => {
+//       const svg = MathJax.tex2svg(x, { display: true });
+//       console.log(MathJax.startup.adaptor.outerHTML(svg));
+//     })
+//     .catch((err) => console.log(err.message));
+//     */
+// }
 
-function inverse(matrix) {
-  const inv = require("./linear_algebra/mat_inv");
-  var steps = inv.elem_transformations(matrix, matrix.length);
-  var y = steps.join("\\\\");
-  console.log(y);
-  /*
-  // var m='matrix([1,2],[3,4])'
-  // var n='matrix([1,2],[3,4])'
-  // var x=require('nerdamer')(m+'*'+n)
-  // console.log(x.toString())
+// function inverse(matrix) {
+//   const inv = require("./linear_algebra/mat_inv");
+//   var steps = inv.elem_transformations(matrix, matrix.length);
+//   var y = steps.join("\\\\");
+//   console.log(y);
+//   /*
+//   // var m='matrix([1,2],[3,4])'
+//   // var n='matrix([1,2],[3,4])'
+//   // var x=require('nerdamer')(m+'*'+n)
+//   // console.log(x.toString())
   
-  require("mathjax")
-    .init({
-      loader: { load: ["input/tex", "output/svg"] },
-    })
-    .then((MathJax) => {
-      const svg = MathJax.tex2svg(y, { display: true });
-      console.log(MathJax.startup.adaptor.outerHTML(svg));
-    })
-    .catch((err) => console.log(err.message));
-  */
-}
+//   require("mathjax")
+//     .init({
+//       loader: { load: ["input/tex", "output/svg"] },
+//     })
+//     .then((MathJax) => {
+//       const svg = MathJax.tex2svg(y, { display: true });
+//       console.log(MathJax.startup.adaptor.outerHTML(svg));
+//     })
+//     .catch((err) => console.log(err.message));
+//   */
+// }
 
+
+
+// const mat = [
+//   [4, 3, 1, 5],
+//   [2, 5, 1, 4],
+//   [3, 3, 2, 1],
+//   [1, 4, 2, 3],
+// ];
+
+// console.log("\nDeterminant:\n")
+// determinant(mat);
+
+// console.log("\nInverse:\n")
+// inverse(mat);
+
+// const mat1 = [
+//   [3, 2, -1],
+//   [1, -1, 5],
+//   [2, 1, 0],
+// ];
+// const mat2 = [1, -2, 3];
+
+// console.log("\nCramer's Rule:\n")
+// cramer(mat1, mat2);
+
+const {transpose,trace,adjoint}=require('./linear_algebra/mat_basics')
 const mat = [
   [4, 3, 1, 5],
   [2, 5, 1, 4],
   [3, 3, 2, 1],
   [1, 4, 2, 3],
 ];
-
-console.log("\nDeterminant:\n")
-determinant(mat);
-
-console.log("\nInverse:\n")
-inverse(mat);
-
-const mat1 = [
-  [3, 2, -1],
-  [1, -1, 5],
-  [2, 1, 0],
-];
-const mat2 = [1, -2, 3];
-
-console.log("\nCramer's Rule:\n")
-cramer(mat1, mat2);
+console.log(transpose(mat)['tex'])
+console.log(trace(mat)['tex'])
+console.log(adjoint(mat,4)['adj'])
